7a0fb77b673ca40c4b286be4739ecc07a5e9d755
SachinDas <107213398+i-am-sachin-root@users.noreply.github.com>
2024-04-05 20:07:35 +0530

    Delete script to list python keywords.py
    test

--- script to list python keywords.py
+++
@@ -1,7 +0,0 @@
-# Python program to demonstrate the application of iskeyword()  
-# importing keyword library which has lists  
-import keyword  
-    
-# displaying the complete list using "kwlist()."  
-print("The set of keywords in this version is: ")  
-print( keyword.kwlist ) 
