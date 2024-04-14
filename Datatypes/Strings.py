21c68a630749e1bbcdbeaa4965be4feca315a606
sachinDas <sachindas353@gmail.com>
2024-04-14 20:30:08 +0530

    adding \n meaning

--- Datatypes/Strings.py
+++ Datatypes/Strings.py
@@ -1,3 +1,4 @@
 #string is sequence of chars like  no and letters in '* ' "* " """*"""
-Mystring = """This is the multiline string 
+Mystring = """This is the multiline string  \n 
 and we are using it to display multiple information on the output"""
+# \n :- used to start output on next line
