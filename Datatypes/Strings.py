1dcbcd2680c5a8508552403ebf3acbb44cde4b63
sachinDas <sachindas353@gmail.com>
2024-04-14 20:35:29 +0530

    Adding indexing meaning in file

--- Datatypes/Strings.py
+++ Datatypes/Strings.py
@@ -2,3 +2,12 @@
 Mystring = """This is the multiline string  \n 
 and we are using it to display multiple information on the output"""
 # \n :- used to start output on next line
+
+# Indeixing :- no of element in the strig 
+""" Forward indexing 
+            A B C D 
+            0 1 2 3 
+             
+    Reverse indexing 
+            A B C D
+        -4 -3 -2 -1 """
