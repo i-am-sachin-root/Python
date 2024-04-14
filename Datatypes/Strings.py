2a56a393f152ebde72b0396c09b0d5a025e11e77
sachinDas <sachindas353@gmail.com>
2024-04-14 20:25:22 +0530

    we have added multiline comments in this

---
+++ Datatypes/Strings.py
@@ -0,0 +1,3 @@
+#string is sequence of chars like  no and letters in '* ' "* " """*"""
+Mystring = """This is the multiline string 
+and we are using it to display multiple information on the output"""
