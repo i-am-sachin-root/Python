9db5c9faf769f23e01dc49a77ee46c9df09a295d
sachinDas <sachindas353@gmail.com>
2024-04-15 17:31:25 +0530

    using fstring to add values in the string

---
+++ Datatypes/fstrings.py
@@ -0,0 +1,11 @@
+"""
+values = x
+f" a string {values}"
+   """
+
+model = "2900/k9"
+slots = 2
+ios = 14.0
+
+print(f"Cisco router model {model} with {slots} gig slots and IOS version of {ios}") #here we are taking values and putting it in fstring
+
