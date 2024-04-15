a0c8086ee39dab763d01c178fd0d91eb0b10df34
sachinDas <sachindas353@gmail.com>
2024-04-15 14:57:51 +0530

    adding 'in' and 'not in' operators

--- Datatypes/string_operators_and_formatting.py
+++ Datatypes/string_operators_and_formatting.py
@@ -1,5 +1,7 @@
 """ + :- this can be used to concatinate string
-    * :- this miltiplies the value    """
+    * :- this miltiplies the value 
+    in :- finds in specified lists, strings etc
+    not in :- reverese of in   """
 
 a = "Saturo "
 b = "Gojo"
@@ -10,4 +12,8 @@ print("concatinating strings using '+' operater :", a+b)
 #Using * operator to multiply string
 print("using * to multiply string m:", a*2)
 
+#Find if value is available in string or not
+print("value in a:",a)
+print("Checking if value is in string or not :","Saturo" in a) #check if the vlaue is matching in string or not
+print("check if the value not in a :","Kenichi" not in a) # check if the value is no matching in string
 
