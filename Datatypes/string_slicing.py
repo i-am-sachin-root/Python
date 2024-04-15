ab914dbdd599ee0d43ca154bc288481778534e60
sachinDas <sachindas353@gmail.com>
2024-04-15 21:06:06 +0530

    final commit of 15 APR 24

--- Datatypes/string_slicing.py
+++ Datatypes/string_slicing.py
@@ -1,3 +1,12 @@
+"""Positve index slicing 
+    string = "somethin to be written"
+    string[0 : 8] :- start and end of the index 
+                    - [ :8] :- start from start and end at 8, vice versa
+
+   Negative index slicing 
+     
+"""
+
 string1 = "O E2   192.168.1.0 [110/20] via 10.0.0.1, 00:00:15, GigabitEthernet0/1"
 
 print("Printing ip address from the routing table :", string1[7:18] ) #here we are using start index and end index to fetch the ip add 
