d22594043759f50242d3f67134f69835415d8fe0
sachinDas <sachindas353@gmail.com>
2024-04-15 16:36:48 +0530

    adding index no in braces to change sequence of string vlaues

--- Datatypes/string_formatting.py
+++ Datatypes/string_formatting.py
@@ -4,7 +4,8 @@
         %d :- holds the integer 
         %f :- holds the float value 
     2) using {} for holding value
-        .format(values):- add values to be hold in braces  """
+        .format(values):- add values to be hold in braces
+    3) using {0} and index no to chose which value needs to be assigned where   """
 
 print("Cisco model: %s,%d WAN slots,IOS %f" % ("2009/k9",2,14.0)) # here we are using placeholders and then values in paras will assigned to them.
 #controling floating points for %f
@@ -13,3 +14,6 @@ print("Cisco model: %s,%d WAN slots,IOS %.f" % ("2009/k9",2,14.0)) # Using %.f w
 
 # We can use {} for hlding the value
 print("Cisco model: {},{} WAN slots,IOS {}".format("2009/k9",2,14.0)) # {} will hold values from the braces
+
+# We will use index no in braces to change sequence of values in string
+print("Cisco model: {1},{0} WAN slots,IOS {2}".format("2009/k9",2,14.0)) #here output will 14, 2009/k9, 14.0
