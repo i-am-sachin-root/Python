98c002b98c163584b0b5996b6a8649389b057f8d
sachinDas <sachindas353@gmail.com>
2024-04-15 20:39:55 +0530

    using splicing to fetch ip add from the string

---
+++ Datatypes/string_slicing.py
@@ -0,0 +1,3 @@
+string1 = "O E2   192.168.1.0 [110/20] via 10.0.0.1, 00:00:15, GigabitEthernet0/1"
+
+print("Printing ip address from the routing table :", string1[7:18] ) #here we are using start index and end index to fetch the ip add 
