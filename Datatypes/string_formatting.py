5ea86f0b2153aff99995efe1efed3557f19077f0
sachinDas <sachindas353@gmail.com>
2024-04-15 16:04:56 +0530

    adding old type of string formatting with '%s,%d,%f' placeholders

---
+++ Datatypes/string_formatting.py
@@ -0,0 +1,7 @@
+# Cisco model: 2900/k9, 2 WAN slots, IOS 14.0
+"""Place holders (old tyoe)
+        %s :- holds the string 
+        %d :- holds the integer 
+        %f :- holds the float value """
+
+print("Cisco model: %s,%d WAN slots,IOS %f" % ("2009/k9",2,14.0)) # here we are using placeholders and then values in paras will assigned to them.
