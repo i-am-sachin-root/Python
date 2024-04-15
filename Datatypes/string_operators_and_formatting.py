68461fc551a4ed65952c9effdbed6a56890ab9f3
sachinDas <sachindas353@gmail.com>
2024-04-15 13:01:20 +0530

    added + and * operators

---
+++ Datatypes/string_operators_and_formatting.py
@@ -0,0 +1,13 @@
+""" + :- this can be used to concatinate string
+    * :- this miltiplies the value    """
+
+a = "Saturo "
+b = "Gojo"
+
+#Using + operator to concatinate strings
+print("concatinating strings using '+' operater :", a+b)
+
+#Using * operator to multiply string
+print("using * to multiply string m:", a*2)
+
+
