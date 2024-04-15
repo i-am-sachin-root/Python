92ba4f0fe55f334faea8ce2d4af850edbfe859dd
sachinDas <sachindas353@gmail.com>
2024-04-15 16:11:11 +0530

    adding %.2f and %.f to control decimal points of fload values

--- Datatypes/string_formatting.py
+++ Datatypes/string_formatting.py
@@ -5,3 +5,6 @@
         %f :- holds the float value """
 
 print("Cisco model: %s,%d WAN slots,IOS %f" % ("2009/k9",2,14.0)) # here we are using placeholders and then values in paras will assigned to them.
+#controling floating points for %f
+print("Cisco model: %s,%d WAN slots,IOS %.2f" % ("2009/k9",2,14.0)) # Using %.2f will allow only 2 decimals after .
+print("Cisco model: %s,%d WAN slots,IOS %.f" % ("2009/k9",2,14.0)) # Using %.f will allow 0 decimals after .
