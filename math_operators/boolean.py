fb33bcde26edf0d16f921f18379421d6c5121fc8
sachinDas <sachindas353@gmail.com>
2024-04-16 13:34:50 +0530

    added and operator

--- math_operators/boolean.py
+++ math_operators/boolean.py
@@ -1,6 +1,8 @@
 """
     Booleans :- TRUE and FALSE
-    
+    AND :- both sides need to be true 
+    OR :- only one side needs to be true 
+    NOT 
     """
 
 num1 = 4
@@ -9,3 +11,8 @@ num2 = 3
 print("are num1 and num2 equal :",num1 == num2 ) # checking if they are equal or not
 print("They are not eaqual :", num1 != num2) # check if they are not equal
 print("Is num1 > than or equal to num2 :", num1 >= num2) # checking return true if num1 is > or == 
+
+# AND operator 
+print("AND operation :", 1==1 and 2==2) # left side and right side = true, if both true then ans = true 
+print("AND operation :", 1==3 and 2==2) # here final value false, because 1 and 3 not equal
+
