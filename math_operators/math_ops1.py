a75590163d9d01eda102bc18647100016e5961ab
sachinDas <sachindas353@gmail.com>
2024-04-16 12:18:42 +0530

    changes done in file and added math operators in 1 file

--- math_operators/math_ops1.py
+++ math_operators/math_ops1.py
@@ -1,5 +1,7 @@
 """
     type() :- tells the datatype of the variable
+    Math operators 
+    +,-,*,/,//,%,**, <,<=,>,=>
     """
 num1 = 22.5
 num2 = 20
@@ -12,4 +14,6 @@ print(num1 // num2) # integer div float part excluded
 print(num1 * num2) # mult
 print(num1 ** num2) # square 
 print(num1 % num2) # remender 
+print("is num1 bigger that num2 :",num1 > num2) # 
+print("is num1 les that num2 :",num1 < num2) # 
 
