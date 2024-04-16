4dfb29e9f1c5a0d9395297d6c77318fc3910b8b1
sachinDas <sachindas353@gmail.com>
2024-04-16 17:10:40 +0530

    using index we changed value in list

--- Lists/list1.py
+++ Lists/list1.py
@@ -1,5 +1,6 @@
 """List is mutable
    List = [] :- an empty list 
+   List[x] = value :- change value in the list by its index no
    """
 
 List1 = ["Cisco","2900/k9",2014,14.0] # added values in list some are string, float and int
@@ -8,3 +9,7 @@ print(List1)
 print(len(List1)) #checking length of list 
 
 print(List1[1]) #will give 1 no in the index 
+
+#changing value in the string 
+List1[2] = "ISR4321" 
+print(List1)
