c8b4c13f11b6922079333e00d1415abb8dd44178
sachinDas <sachindas353@gmail.com>
2024-04-16 17:03:52 +0530

    added len() and indexing to get length of the list and specific value using index no

---
+++ Lists/list1.py
@@ -0,0 +1,10 @@
+"""List is mutable
+   List = [] :- an empty list 
+   """
+
+List1 = ["Cisco","2900/k9",2014,14.0] # added values in list some are string, float and int
+print(List1)
+
+print(len(List1)) #checking length of list 
+
+print(List1[1]) #will give 1 no in the index 
