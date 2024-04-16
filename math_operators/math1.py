b730a44c935e3815a4d82b70256eb8299dbb4ee5
sachinDas <sachindas353@gmail.com>
2024-04-16 12:06:06 +0530

    adding + - * / // ops in file

---
+++ math_operators/math1.py
@@ -0,0 +1,12 @@
+"""
+    type() :- tells the datatype of the variable
+    """
+num1 = 20 
+num2 = 22.5
+
+
+print(num1 + num2) #addidtion
+print(num1 - num2) #sub
+print(num1 / num2) #div
+print(num1 // num2) # integer div float part excluded
+print(num1 * num2) # mult
