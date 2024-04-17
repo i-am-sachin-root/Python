17e70d621a149122e92b023004d054a6be616482
sachinDas <sachindas353@gmail.com>
2024-04-17 11:50:38 +0530

    added min and max func to find min and max values from list

---
+++ Lists/list_method.py
@@ -0,0 +1,10 @@
+"""
+min(list) :- gives minimum values from the list 
+max(list) :- gives max value from the list 
+"""
+
+List1 = ["cisco", "google", 2900,14.0]
+List2 = [1,2,43,-3,0]
+
+print("min() will give min value from the list :",min(List2)) #find minimum value from the list
+print("max() will give max value from list :", max(List2)) # finding max value from the list 
