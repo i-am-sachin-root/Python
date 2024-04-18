94cec615cab0c382ffd1f8cf6674710b8ffbaddf
sachinDas <sachindas353@gmail.com>
2024-04-18 16:15:36 +0530

    added diff between list,set,tuple,dict,ranges

---
+++ diff.py
@@ -0,0 +1,9 @@
+
+"""
+Data Structure	Ordered	    Mutable	    Duplicates	Syntax
+Lists	        Yes	        Yes	        Yes	        []
+Sets	        No	        Yes	        No	        {} or set()
+Tuples	        Yes	        No	        Yes	        ()
+Ranges	        No	        No	        No	        range()
+Dictionaries	No	        Yes	        Keys only	{key1: value1, key2: value2, ...} \
+"""
