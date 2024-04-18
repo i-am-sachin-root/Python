7ae38aaf9ea358174b548a41032fa316de4e5243
sachinDas <sachindas353@gmail.com>
2024-04-18 17:18:35 +0530

    created set with set()

---
+++ sets/set1.py
@@ -0,0 +1,16 @@
+"""
+Mathod 1 for set creation
+set1 =  [1,2,3,4,5]
+set1 = set(set1) :- using function to create set
+
+Methid 2 for set creation 
+set1 = {1,2,3,4,5}
+
+imp :- we cannot repeate values in set 
+"""
+
+#Creating set 
+set1 =  [1,2,3,4,5,5]
+set1 = set(set1) # here we have to assign itself to get set otherwise it will be a list 
+print(set1)
+
