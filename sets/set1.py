2b7d5e911fdd4244dc6326076f5ac35c84ebaf5e
sachinDas <sachindas353@gmail.com>
2024-04-19 09:31:33 +0530

    added {} for creating set

--- sets/set1.py
+++ sets/set1.py
@@ -9,8 +9,12 @@ set1 = {1,2,3,4,5}
 imp :- we cannot repeate values in set 
 """
 
-#Creating set 
+#Creating set using set()
 set1 =  [1,2,3,4,5,5]
 set1 = set(set1) # here we have to assign itself to get set otherwise it will be a list 
 print(set1)
 
+
+#creating set using {}
+set2 = {12,13,13,14,15}
+print("creating set using { } ", set2)
