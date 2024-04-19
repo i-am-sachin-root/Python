02296a6d469d75093b28a4cb2df7fd0463b55104
sachinDas <sachindas353@gmail.com>
2024-04-19 12:00:06 +0530

    finding same values in sets by using intersecion()

---
+++ sets/set_methods.py
@@ -0,0 +1,10 @@
+"""
+set.intersection() :- find the same elements in the set 
+
+"""
+
+set1 = {1,2,3,4,5,6}
+set2 = {4,5,6,7,8}
+
+set1 = set1.intersection(set2) # finding same values in sets by using intersecion()
+print("Common elements usng intersection() :", set1)
