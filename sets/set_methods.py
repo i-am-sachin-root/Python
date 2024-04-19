4ee828c24ab673fa3c756ee8c88e837fef8e3ac0
sachinDas <sachindas353@gmail.com>
2024-04-19 17:51:38 +0530

    added union() to combine both sets into one

--- sets/set_methods.py
+++ sets/set_methods.py
@@ -1,6 +1,7 @@
 """
 set.intersection() :- find the same elements in the set 
 set1 = set1.difference(set2) :- compare one with other and find diff in one 
+set.union :- add all sets together
 """
 
 set1 = {1,2,3,4,5,6}
@@ -14,10 +15,11 @@ print("Common elements usng intersection() :", set1)
 set1 = {1,2,3,4,5,6}
 set2 = {4,5,6,7,8}
 set1 = set1.difference(set2) # if interchange then it will give 7,8
-print(set1)
+print("diff compared to set2 in set1 :",set1)
 
 # add diff set in one with union 
 set1 = {1,2,3,4,5,6}
 set2 = {4,5,6,7,8}
 set1 = set1.union(set2)
 print("Adding both sets with union() method :", set1)
+
