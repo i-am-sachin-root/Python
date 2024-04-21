315ccf3b050b6c90ea06980eb01284e39df3b8e9
sachinDas <sachindas353@gmail.com>
2024-04-21 16:14:18 +0530

    added single tuple element using ','

---
+++ tuples/tuples.py
@@ -0,0 +1,12 @@
+"""
+immutable list 
+it is ordered
+IMP :- used to keep data is sequence and unchangable 
+indexes and duplicates are allowed
+ """
+
+#create single element tuple, normally tuple has many elements 
+single_tuple = (1,)
+print(type(single_tuple))
+print("Creating tuple with single element just add ',' after element :",single_tuple)
+
