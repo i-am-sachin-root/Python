35375f28fe85aba068e512a3a5a9a0494f494167
sachinDas <sachindas353@gmail.com>
2024-04-21 12:42:30 +0530

    added add() and remove() which can't work on frozenset, added intersection,union,diff methods

--- sets/frozensets.py
+++ sets/frozensets.py
@@ -1,11 +1,30 @@
 """
-Frozen sets are immutable 
+Frozen sets are immutable, we cannot edit frozenset 
+but we can take them and work for diff output line using intersection(),difference(),union()
 """
 
-list1 = {1,2,3,4}
-list2 = {3,4,7}
+fset1 = {1,2,3,4}
+fset2 = {3,4,7}
 
-list1 = frozenset(list1)
-list2 = frozenset(list2)
+fset1 = frozenset(fset1)
+fset2 = frozenset(fset2)
 
-print("checking type of set :", type(list1))
+print("checking type of set :", type(fset1))
+
+#fset1 = fset1.remove(2)
+#print("Trying to remove frozensets element :",fset1) here we are removing frozenset element but we cannot 
+
+#using intersection on sets 
+fset1 = fset1.intersection(fset2)
+print("intersection", fset1)
+
+#using difference 
+fset1 = {1,2,3,4}
+fset2 = {3,4,7}
+fset1 = fset1.difference(fset2) # checking difference in fset1 compared to fset2 
+print("Differece :",fset1)
+
+#using union 
+fset1 = fset1.union(fset2)
+print("union :",fset1)
+
