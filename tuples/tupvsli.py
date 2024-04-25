df834c1d36a0455e7011ff3612bb5ae3903fadce
sachinDas <sachindas353@gmail.com>
2024-04-25 11:56:47 +0530

    added dir() to find out which type of functions and methods can be used on datatypes

--- tuples/tupvsli.py
+++ tuples/tupvsli.py
@@ -6,4 +6,12 @@ Tuples :- immutable
 Lits :- Mutbale
         can be modified 
         variable length
-         [] """
+         [] 
+         
+         dir() :- see the methods availbale for the datatypes """
+
+
+a = () #empty tuple 
+b = [] #empty list
+
+print(dir(a))  # printing methods which can be applied on the tuple
