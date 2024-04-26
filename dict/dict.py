4ee3b35fd73995266ad959caecc318d6c890a49e
sachinDas <sachindas353@gmail.com>
2024-04-26 11:03:01 +0530

    added emty dict

---
+++ dict/dict.py
@@ -0,0 +1,12 @@
+"""Dictioneries - unorders set of key vlaue pairs
+                - seperated by ,
+                - {}
+                - key: pair
+                - mutable we can edit the dict 
+                
+                
+"""
+
+#creating emoty dict 
+dict1 = {}
+print(type(dict1))
