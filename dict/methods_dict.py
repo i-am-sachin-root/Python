0c6cee3b04582d0134794e0fa3e7ed13ea31cd4b
sachinDas <sachindas353@gmail.com>
2024-04-26 13:13:03 +0530

    exctracted some values using slicing with their key

---
+++ dict/methods_dict.py
@@ -0,0 +1,2 @@
+dict2 = {"Vendor": "Cisco","Model":"2900","IOS":"12","gig ports":"2","FE ports":"4"}
+print("Printing no of gig ports:",(dict2["gig ports"])) #this will print the value of the gig ports key 
