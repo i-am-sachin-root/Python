8b3bccf4ac354c890743a904b6cfda62771ed5ba
sachinDas <sachindas353@gmail.com>
2024-04-26 13:27:38 +0530

    deleted the key pair using 'del' method

--- dict/methods_dict.py
+++ dict/methods_dict.py
@@ -4,3 +4,11 @@ print("Printing no of gig ports:",(dict2["gig ports"])) #this will print the val
 #adding new key pair in dict
 dict2["RAM"] = "256 MB"
 print("Adding new key: pair in dict :",dict2)
+
+#modefying the vlaue from the dict 
+dict2["RAM"] = "512" # lets say we added extra dim of 256 
+print("Modefied key vlaue :",dict2)
+
+#deleting the key pair from the dict
+del dict2["RAM"]
+print("Deleted RAM key pair from the dict :",dict2)
