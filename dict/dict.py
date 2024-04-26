bf63d9fdaa2782cf619ae8d7e17587fc89a31224
sachinDas <sachindas353@gmail.com>
2024-04-26 11:13:15 +0530

    added dict2 and key pair vlaues

--- dict/dict.py
+++ dict/dict.py
@@ -7,6 +7,9 @@
                 
 """
 
-#creating emoty dict 
+#creating empty dict 
 dict1 = {}
 print(type(dict1))
+
+dict2 = {"Vendor": "Cisco","Model":"2900","IOS":"12","gig ports":"2","FE ports":"4"}
+print(dict2)
