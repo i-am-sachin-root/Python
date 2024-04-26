54c9d2015c735b63984851b815eba2d276c8cd79
sachinDas <sachindas353@gmail.com>
2024-04-26 13:19:27 +0530

    adding new key:pair in the dict

--- dict/methods_dict.py
+++ dict/methods_dict.py
@@ -1,2 +1,6 @@
 dict2 = {"Vendor": "Cisco","Model":"2900","IOS":"12","gig ports":"2","FE ports":"4"}
 print("Printing no of gig ports:",(dict2["gig ports"])) #this will print the value of the gig ports key 
+
+#adding new key pair in dict
+dict2["RAM"] = "256 MB"
+print("Adding new key: pair in dict :",dict2)
