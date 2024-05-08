b2bf34f6851ad5bbcd50b7dd955ddcb81133a4c3
sachinDas <sachindas353@gmail.com>
2024-05-08 11:15:13 +0530

    changed the comparison value in if from 5 to 15 so 2nd block can run

--- conditions_loops_exceptions/if_elif_else.py
+++ conditions_loops_exceptions/if_elif_else.py
@@ -10,7 +10,7 @@ else epression
 
 x =  10 
 
-if x  > 5: #if matches then print inside / run inside code, if not then go to next block
+if x  > 15: #if matches then print inside / run inside code, if not then go to next block
     print("X is greater than 5") 
 elif x == 10: #if matches then print inside / run inside code, if not then go to next block
     print("x is equal to 10")
