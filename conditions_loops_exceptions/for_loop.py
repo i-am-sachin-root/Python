9ff69e5f54d7a2450d3829f0de1c57f26fd2f22d
sachinDas <sachindas353@gmail.com>
2024-05-08 16:30:01 +0530

    added for and used list to assign elements to variable in for loop to check in list and perform operations on it

---
+++ conditions_loops_exceptions/for_loop.py
@@ -0,0 +1,8 @@
+"Used for iteration on the sequence "
+list1 = ["Cisco","Juniper","Palo Alto","Fortinet"]
+print("checking vcariable type",type(list1))
+
+
+for i in list1: # here we perform the operation on the list elements one by one, one element taken in place of 'i' and inside block if perfomred
+        #print(list1) # this will print the whole list 4 times
+        print(i) # this will print elements one by one 
