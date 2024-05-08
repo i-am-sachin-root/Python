49befa985ed1b49cb5ed2253b6d0c4e19d70dda6
sachinDas <sachindas353@gmail.com>
2024-05-08 11:12:24 +0530

    added if elif else statement

--- conditions_loops_exceptions/if_elif_else.py
+++ conditions_loops_exceptions/if_elif_else.py
@@ -10,8 +10,10 @@ else epression
 
 x =  10 
 
-if x  > 5:
-    print("X is greater than 5:")
-else:
+if x  > 5: #if matches then print inside / run inside code, if not then go to next block
+    print("X is greater than 5") 
+elif x == 10: #if matches then print inside / run inside code, if not then go to next block
+    print("x is equal to 10")
+else: #if non of the above matches then run inside code
     print("x is not > than 5")
 
