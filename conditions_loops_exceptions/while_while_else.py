c8882b05faa377ca23423a6f46c1aedce4a1f29c
sachinDas <sachindas353@gmail.com>
2024-05-12 11:27:29 +0530

    added infinite loop in while loop

--- conditions_loops_exceptions/while_while_else.py
+++ conditions_loops_exceptions/while_while_else.py
@@ -5,9 +5,16 @@
 x = 1  # initilaization of the 1st value 
 
 print("Printing the range till while condtions not matched")
+while x <= 10: # while is checking x = 1 against 1 <= 10 and the ans is true, and running inside code.
+    print(x) # print the 1st value 1 
+    x = x + 1 # adding 1 + 1 + 2, x = 2 and now the loop continues. 
+
+
+# what would happen if we do not add the increment line 
+x = 1 
 while x <= 10:
     print(x)
-    x = x + 1 
+    # here we don't specify x = x + 1, it'll never add up to 10 <= 10 to end the while loop, so while loop will run infinetly 
        
        
     
