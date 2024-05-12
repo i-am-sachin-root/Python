2f5795a08997ba2b0d17873d64ed4a6184c5af07
sachinDas <sachindas353@gmail.com>
2024-05-12 11:16:56 +0530

    added while loop and print range 10 till condiotion matched

---
+++ conditions_loops_exceptions/while_while_else.py
@@ -0,0 +1,15 @@
+""" while block executes the code block as long as the defined condition is true 
+- if the condition stays true that means it will run infinatly 
+- it will run as long as the condtions remains true, the moment conditions becomes false """
+
+x = 1  # initilaization of the 1st value 
+
+print("Printing the range till while condtions not matched")
+while x <= 10:
+    print(x)
+    x = x + 1 
+       
+       
+    
+
+
