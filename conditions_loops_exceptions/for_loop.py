0d891b55bdfc9b7ac9d44ec6250b83f68b08eeef
sachinDas <sachindas353@gmail.com>
2024-05-12 10:28:48 +0530

    added enumerate() func to display index and elements of the list using for loop, it'll iterate on list element by element

--- conditions_loops_exceptions/for_loop.py
+++ conditions_loops_exceptions/for_loop.py
@@ -6,3 +6,13 @@ print("checking vcariable type",type(list1))
 for i in list1: # here we perform the operation on the list elements one by one, one element taken in place of 'i' and inside block if perfomred
         #print(list1) # this will print the whole list 4 times
         print(i) # this will print elements one by one 
+
+r = range(10)
+for i in r:
+        print("printing the range of r using for loop:",i)
+
+
+
+list1 = ["Cisco","Fortinet","DELL","HP","google"]
+for (index, element) in  enumerate(list1): # here we are printing the index as well as their elements 
+        print(index,element)
