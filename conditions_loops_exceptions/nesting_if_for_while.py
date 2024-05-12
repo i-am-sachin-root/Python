d9328699db33572d273d5469d99336669b8a36ad
sachinDas <sachindas353@gmail.com>
2024-05-12 12:12:38 +0530

    added nested if statement

---
+++ conditions_loops_exceptions/nesting_if_for_while.py
@@ -0,0 +1,7 @@
+"control flow statements:- if, for, while"
+
+# nesting the if statements 
+x = "cisco"
+if "i" in x: # checking if i exists in the string 
+    if len(x) > 3: #checking if string is greater than 
+        print(x,len(x)) # if both if's are true then print 
