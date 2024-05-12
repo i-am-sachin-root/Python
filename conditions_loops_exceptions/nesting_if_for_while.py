45ebb7f28a47e3552bdb7b1edb26471aa91c9c53
sachinDas <sachindas353@gmail.com>
2024-05-12 12:20:31 +0530

    used and operator to get same output, skipped using nested if

--- conditions_loops_exceptions/nesting_if_for_while.py
+++ conditions_loops_exceptions/nesting_if_for_while.py
@@ -5,3 +5,8 @@ x = "cisco"
 if "i" in x: # checking if i exists in the string 
     if len(x) > 3: #checking if string is greater than 
         print(x,len(x)) # if both if's are true then print 
+
+# this above code can be written susing and operator 
+x = "cisco"
+if ("i" in x) and (len(x) > 3):
+    print(x,len(x))
