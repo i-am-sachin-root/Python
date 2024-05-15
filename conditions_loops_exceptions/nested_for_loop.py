9ef0d8df74c14a8ea6b72c965ec1e8933d4e63fd
sachinDas <sachindas353@gmail.com>
2024-05-15 10:54:28 +0530

    added nested for loop inside loop well complete and then main loop will go to its next element

---
+++ conditions_loops_exceptions/nested_for_loop.py
@@ -0,0 +1,10 @@
+list1 = [4,5,6,]
+list2 = [7,8,9]
+
+for i in list1: #taking 1 element from the list1
+    for j in list2: # taking 1 element from the list2 
+        print(i * j) #mutlipying elements both list elements sequence vise
+
+""" here list1 i(4) x list2 j(7,8,9) will be multiplied
+list2 elements will be multiplied with list1's 1st element till list2 elements are completed, only then will list1 use it's next element """
+
