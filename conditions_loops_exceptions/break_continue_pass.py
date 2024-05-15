c0292d319c9d7e493bc0f6443f10b7c599f9b675
sachinDas <sachindas353@gmail.com>
2024-05-15 11:05:03 +0530

    added break statement in the for loop to terminate it

---
+++ conditions_loops_exceptions/break_continue_pass.py
@@ -0,0 +1,9 @@
+""" break and continue used in for and while loop to control the flow"""
+
+# break :- used to terminate loop in which is resides
+
+for no in range(10): #print no 0 to 9
+    if no == 7: #checking the no is 7 or not 
+        break #if no value is 7 then break the code 
+    print(no) #print the last value of the no var
+        
