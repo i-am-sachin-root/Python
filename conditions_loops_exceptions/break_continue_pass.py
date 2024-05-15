6f458a103cfdf4e73b6501ff6e24a8bfeae30c85
sachinDas <sachindas353@gmail.com>
2024-05-15 11:19:32 +0530

    added break inside nested for loop

--- conditions_loops_exceptions/break_continue_pass.py
+++ conditions_loops_exceptions/break_continue_pass.py
@@ -7,3 +7,13 @@ for no in range(10): #print no 0 to 9
         break #if no value is 7 then break the code 
     print(no) #print the last value of the no var
         
+
+list1 = [4,5,6]
+list2 = [10,20,30]
+
+for i in list1: #taking element from list1 
+    for j in list2: #taking element from list2
+        if j == 20: #checking if the lj var has reached list2 20 element 
+            break # if j = 20 then break the loop
+        print(i * j)
+    print("outside of the isnide for loop")
