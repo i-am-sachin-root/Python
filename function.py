e3ba6dc361c7df9a08b90e221dc7fa7b53323423
sachinDas <sachindas353@gmail.com>
2024-05-25 13:35:18 +0530

    added test function

---
+++ function.py
@@ -0,0 +1,9 @@
+"""function is a block of code that can be reused 
+def :- is used to define a function
+functions stores the block of code that does specific task and can be called multiple times in a programm"""
+
+def test_function():
+    "Printing hello world"
+    print("Hello world")
+
+help(test_function)
