5bb6695fbee949d304f9a79e5fbf80325614999e
sachinDas <sachindas353@gmail.com>
2024-05-25 11:26:47 +0530

    added excepations like NameError and how they work

---
+++ exceptions/excepations.py
@@ -0,0 +1,23 @@
+"""
+two types of error
+syntax error:- when we don't follow the language rules
+exceptions:- exceptions are reaised while running the program and they interrupt the flow of the programm """
+
+# syntax error 
+for i in range(10): # this will give syntax error because the block of for loop is empty 
+        pass #adding pass statement to avoid the syntax error 
+
+
+#creating NameError 
+for i in ange(10): #This will create name error 
+        print(i)
+
+
+
+
+ 
+ 
+
+
+
+    
