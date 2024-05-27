b97a296528817a758f2137220546149e4d2e68bd
sachinDas <sachindas353@gmail.com>
2024-05-27 09:14:59 +0530

    added docstring meaning and calling the function in interpreter

---
+++ functions/function.py
@@ -0,0 +1,11 @@
+"""function is a block of code that can be reused 
+def :- is used to define a function
+functions stores the block of code that does specific task and can be called multiple times in a programm"""
+
+def test_function():
+    "Printing hello world" # this is called docstring, here we describe what this function does 
+    print("Hello world")
+
+help(test_function) #by this we can see the functions docstring.
+test_function()
+
