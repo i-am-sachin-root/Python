722b5b26e419ba826dced82c147b56e25ecca0e5
sachinDas <sachindas353@gmail.com>
2024-05-27 09:42:07 +0530

    added multiple parameters and used fucntion to add value to those parameters and called the function

--- functions/parameters.py
+++ functions/parameters.py
@@ -2,3 +2,10 @@ def test_function(x): #adding parameters in function
     print(x)
 
 test_function("good girl") #after calling the function the we have added a string in the function and this string will go to x parameter of the fucntion, and execute the print(x).
+
+# Adding multiple parameters in function
+def test_function2(x,y):
+    print("Hello " + x)
+    print("Hello " + y)
+
+test_function2("Sachin","Jack")
