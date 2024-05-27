d3369958ec4c03bb69985aabff5a3f5138200f46
sachinDas <sachindas353@gmail.com>
2024-05-27 09:35:08 +0530

    added function parameters and added value in function after calling it, and executed the funbction block'

---
+++ functions/parameters.py
@@ -0,0 +1,4 @@
+def test_function(x): #adding parameters in function
+    print(x)
+
+test_function("good girl") #after calling the function the we have added a string in the function and this string will go to x parameter of the fucntion, and execute the print(x).
