2ec15feacc16f474cefbdd5e1a1748096bf6b6da
sachinDas <sachindas353@gmail.com>
2024-06-01 09:53:11 +0530

    imported math module and extracted value of pi from the math module

---
+++ modules/importing.py
@@ -0,0 +1,5 @@
+import math  #importing module 
+print(dir(math)) #print the content of the module 
+
+# using var or function from the math module 
+print(math.pi) # here we are extracting value of pie from the math module
