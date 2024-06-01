e3137cd40cc5f8a68759aac3549f93073f3d92ef
sachinDas <sachindas353@gmail.com>
2024-06-01 12:27:32 +0530

    added where python seraches the modules and path variables to find modules

--- modules/from_import_statement.py
+++ modules/from_import_statement.py
@@ -1,2 +1,11 @@
-from test_module import var1 #here we are using only what we need from the module
+from test_module import var1 #here we are using only what we need from the module, using * means import all  
+import sys   
 print(var1)
+
+""""
+Module search for import :- 1) build in modules are searched
+                            2) searched in current dict
+                            3) searched in path variables  """
+
+#check available path variable where python finds its imp files
+print(sys.path)
