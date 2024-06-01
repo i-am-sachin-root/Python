ac4636500b9d2db8bd35125d475f271add502d5d
sachinDas <sachindas353@gmail.com>
2024-06-01 10:27:44 +0530

    called modules function in the main code

--- modules/test_app.py
+++ modules/test_app.py
@@ -2,3 +2,5 @@ import test_module  # type: ignore
 # this is main code 
 print("main code ")
 print(test_module.var1) #here we are exctracting var1 from test_module.py
+
+print(test_module.func1()) #using function from our test module 
