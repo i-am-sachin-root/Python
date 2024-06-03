9d4d3f7b37001f89155ee96417998da46ee6ce08
sachinDas <sachindas353@gmail.com>
2024-06-03 10:03:29 +0530

    added regular expression file

---
+++ regex/regex.py
@@ -0,0 +1,5 @@
+"""regular expression helps to find paatter inside a string 
+Regular expression module:- re"""
+import re 
+mystr = "You can learn any programming language, whether it is Python2, Python3, Pearl, Java, javascript or PHP."
+
