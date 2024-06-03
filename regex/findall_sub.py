c090042b7f1be7ce0c91282fa207307ecc67aab4
sachinDas <sachindas353@gmail.com>
2024-06-03 12:53:54 +0530

    final findall

--- regex/findall_sub.py
+++ regex/findall_sub.py
@@ -7,3 +7,7 @@ a = re.findall(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", arp) # ip add expression 1
 print(a)
 
 
+# sub()
+#used to replace in the string using regex,chages,stringname. we can change using regular expression 
+b = re.sub(r"\d", "0", arp) # change all no in the arp var into 0
+print(b)
