1699b43e7d934f4b1bdb828be198902cbadfbbd9
sachinDas <sachindas353@gmail.com>
2024-06-03 10:19:08 +0530

    added match() method to find starting matching characters in the string

--- regex/regex.py
+++ regex/regex.py
@@ -3,3 +3,13 @@ Regular expression module:- re"""
 import re 
 mystr = "You can learn any programming language, whether it is Python2, Python3, Pearl, Java, javascript or PHP."
 
+# match() method, finds matching at the begining of the scene
+#sysntax
+"""
+var = re.match(pattern, string, optional flags"""
+var1 = re.match("You",mystr)
+print(var1)
+
+#ignore cases in the string
+var1 = re.match("you", re.I)
+print(var1)
