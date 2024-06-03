900972de80c8fa49ccffb0c694d89b2dcd162898
sachinDas <sachindas353@gmail.com>
2024-06-03 13:50:18 +0530

    added \W for non word

--- regex/extra_regex.py
+++ regex/extra_regex.py
@@ -14,3 +14,8 @@ print(t)
 #\S no whitespace 
 t = re.search(r"\S+",a) #will stop after 1st white space becase it is inverse of finding white space
 print(t)
+
+
+#\W non word, will ignore words no and _
+t = re.search(r"\W",a) #this will give 1st whitespace, can find all white spaces but need to use findall()
+print(t)
