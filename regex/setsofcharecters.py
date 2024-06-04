52e9cc34e3ddec3ce2d797cf8681ce84cac66c5c
sachinDas <sachindas353@gmail.com>
2024-06-04 10:09:57 +0530

    added regex to find letters from a to z, exclude letters if we want to while searching

--- regex/setsofcharecters.py
+++ regex/setsofcharecters.py
@@ -1,5 +1,16 @@
 import re 
-a = "123 is a number, and 1234 is another number"
+a = "123 Is a number, And 1234 is another number"
 res = re.findall(r"[a-z]",a) #this will seperate the charecters and print in the list 
 print(type(res))
+print(res)
+
+res = re.findall(r"[A-Z]",a) # this will find capital letters 
+print(res)
+
+res = re.findall(r"[ab]",a) # this will find a and b occurances  letters 
+print(res)
+
+
+
+res = re.findall(r"[^a]",a) # this will find all but ignore a  
 print(res)
