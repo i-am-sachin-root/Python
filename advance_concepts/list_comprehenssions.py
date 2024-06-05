305eacefa60403b223f7417e9689a0ed2db6158e
sachinDas <sachindas353@gmail.com>
2024-06-05 10:19:59 +0530

    added list comprahenssion

--- advance_concepts/list_comprehenssions.py
+++ advance_concepts/list_comprehenssions.py
@@ -1,5 +1,10 @@
+#creating for loop to find spr till 10
 list1 = []
 for i in range(10):
     result = i ** 2 
     list1.append(result)
 print(list1)
+
+# with list comprahenssion 
+list2 = [x ** 2 for x in range(10)] #formula for x till 10 
+print(list2)
