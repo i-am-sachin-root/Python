66a2b4f85f27eaff1cb4eb54f0aee83224a5fd96
sachinDas <sachindas353@gmail.com>
2024-06-05 10:49:28 +0530

    created for set also

--- advance_concepts/list_comprehenssions.py
+++ advance_concepts/list_comprehenssions.py
@@ -8,3 +8,10 @@ print(list1)
 # with list comprahenssion 
 list2 = [x ** 2 for x in range(10)] #formula for x till 10 
 print(list2)
+
+#with if 
+list3 = [x ** 2 for x in range(10) if x > 5]  #only will print 
+
+
+#for set comprehenssion
+set1 = {x ** 2 for x in range(10)}
