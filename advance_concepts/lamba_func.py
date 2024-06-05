ea330c8dedd181c19c0f93c8723599fd04ce0dfc
sachinDas <sachindas353@gmail.com>
2024-06-05 11:22:30 +0530

    last lambd commit

---
+++ advance_concepts/lamba_func.py
@@ -0,0 +1,7 @@
+"small function which are created during runtime called lambda functions"
+"Dont't need return statement to output from this fucntion"
+"Used where we cannot use regular def functions"
+
+#creating basic lambda function
+a = lambda x,y : x*y
+print(a(5,4))
