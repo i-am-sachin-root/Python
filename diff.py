727a40c9fe471431a31a7056d53f5a5d66745497
SachinDas <107213398+i-am-sachin-root@users.noreply.github.com>
2024-06-06 11:49:36 +0530

    Made table Update diff.py easy to
    Read

--- diff.py
+++ diff.py
@@ -1,9 +1,12 @@
 
-"""
-Data Structure	Ordered	    Mutable	    Duplicates	Syntax
-Lists	        Yes	        Yes	        Yes	        []
-Sets	        No	        Yes	        No	        {} or set()
-Tuples	        Yes	        No	        Yes	        ()
-Ranges	        No	        No	        No	        range()
-Dictionaries	No	        Yes	        Keys only	{key1: value1, key2: value2, ...} \
-"""
+
+# Data Structure	Ordered	    Mutable	    Duplicates	 Syntax
+# Lists	            Yes	        Yes	        Yes	         []
+# Sets	            No	        Yes	        No	         {} or set()
+# Tuples	        Yes	        No	        Yes	         ()
+# Ranges	        No	        No	        No	         range()
+# Dictionaries	    No	        Yes	        Keys only	 {key1: value1, key2: value2, ...} \
+ 
+
+
+
